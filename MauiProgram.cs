e985f3a3044ab21251928547cb8627defb5777e5
grishaev <grishaev@aeroclub.ru>
2024-08-28 00:24:05 +0300

    Добавил MoveAndResize для windows

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
 
 namespace maui.boozer
 {
@@ -16,8 +22,34 @@ namespace maui.boozer
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
+
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(wndLifeCycleBuilder =>
+                {
+                    wndLifeCycleBuilder.OnWindowCreated(window =>
+                    {
+                        IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+                        AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);
+                        if (winuiAppWindow.Presenter is OverlappedPresenter p)
+                        {
+                            const int width = 1080;
+                            const int height = 2400;
+                            const float scale = 0.5f;
+
+                            p.IsMinimizable = false;
+                            p.IsResizable = false;
+
+                            winuiAppWindow.MoveAndResize(new RectInt32(110, 120, (int)(width * scale), (int)(height * scale)));
+                        }
+                    });
+                });
+            });
+#endif     
 
             return builder.Build();
         }
